72be897bacee8e06087056b8fcaaa760503391b5
btravers <benoit.travers@insa-rennes.fr>
2014-12-06 16:19:58 +0100

    Splitter: execute new query

--- Decomposition/src/federatedb/Splitter.java
+++ Decomposition/src/federatedb/Splitter.java
@@ -9,6 +9,10 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.xml.xquery.XQException;
+
+import tools.XQueryExecutioner;
+
 /**
  * 
  * @author btravers
@@ -93,6 +97,12 @@ public class Splitter {
 
 		}
 		
+		try {
+			XQueryExecutioner.executeQuery(this.query);
+		} catch (XQException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
 
