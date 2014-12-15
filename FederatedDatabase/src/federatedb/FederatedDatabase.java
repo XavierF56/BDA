ece5d0d1fd25014ee819048ff1f8d443772d5a99
XavierF56 <xavier.fraboulet@insa-rennes.fr>
2014-12-15 10:23:25 +0100

    end

--- FederatedDatabase/src/federatedb/FederatedDatabase.java
+++ FederatedDatabase/src/federatedb/FederatedDatabase.java
@@ -35,40 +35,51 @@ public class FederatedDatabase {
 		}
 	}
 	
+	public void cli(String[] args) throws Exception {
+		if(args[0].equals("-q")) {
+			if (args.length == 1 || args.length > 3) {
+				System.out.println("Wrong options: -h to review available options");
+				return;
+			}
+			
+			String query = args[1];
+			String output = "result.xml";
+			if (args.length == 3)
+				output = args[2];
+				
+			this.query(query, output);
+		} else if (args[0].equals("-m")) {
+			this.getModel();
+		} else if (args[0].equals("-h")) {
+			System.out.println("Available options:\n" +
+					"   -q <query_file> (<output_file>) \n" +
+					"         query the Federated database\n" +
+					"   -m    display database model\n" +
+					"   -h    display help message\n");
+		}else {
+			System.out.println("Wrong options: -h to review available options");
+		}
+	}
+	
 	
 	public static void main(String[] args) throws Exception {
 		List<IWrapper> wrappers = new ArrayList<IWrapper>();
 		wrappers.add(new XMLWrapper("sourcesXML", "XML"));
 		wrappers.add(new SqlWrapper("sourcesSQL/fournisseur", "SQL"));
-		// /!\ Path for windows
 		
 		FederatedDatabase fdb = new FederatedDatabase(wrappers);
 		
-		if (args.length == 0) {
-			fdb.query("query.xq", "result.xml");
-			//fdb.getModel();
+		// default query/output
+		String query = "Requetes/complexe1XML1SQL.xq";
+		String output = "Requetes/result.xml";
+		
+		if (args.length > 0) {
+			// Command line interface mode
+			fdb.cli(args);
 		} else {
-			if(args[0].equals("-q")) {
-				if (args.length == 1 || args.length > 3)
-					System.out.println("Wrong options: -h to review available options");
-				
-				String query = args[1];
-				String output = "result.xml";
-				if (args.length == 3)
-					output = args[2];
-					
-				fdb.query(query, output);
-			} else if (args[0].equals("-m")) {
-				fdb.getModel();
-			} else if (args[0].equals("-h")) {
-				System.out.println("Available options:\n" +
-						"   -q <query_file> (<output_file>) \n" +
-						"         query the Federated database\n" +
-						"   -m    display database model\n" +
-						"   -h    display help message\n");
-			}else {
-				System.out.println("Wrong options: -h to review available options");
-			}
-		}	
+			// default mode
+			fdb.query(query, output);
+			//fdb.getModel();
+		}
 	}
 }
